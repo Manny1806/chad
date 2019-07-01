be6eb2d34ed41768aaf530b0779c995ec6ab5b3f
Alex Widner <manny1806@gmail.com>
2019-07-01 00:39:30 -0700

    slider switching functionality working

--- chad-website/src/App.js
+++ chad-website/src/App.js
@@ -6,29 +6,94 @@ export class App extends React.Component {
 
   constructor(props) {                                                                                                                                                                                                                                                                                                                                                                                                                
     super(props);
-    // Don't call this.setState() here!
     this.state = { 
       bandOpacity: 0,
       musicOpacity: 0,
       photosOpacity: 0,
       eventsOpacity: 0,
       contactOpacity: 0,
-      currentBandMember: "none",
+      currentBandIndex: null,
       currentBandSlide: 1,
-      bandSlideDirection: "right"
+      bandSlideDirection: "right",
+      bandSlide1: "",
+      bandSlide2: ""
     };
+    
     this.componentDidMount = this.componentDidMount.bind(this)
-    this.meetTheBand = this.meetTheBand.bind(this)
-    this.nextBandMember = this.nextBandMember.bind(this)
-    this.previousBandMember = this.previousBandMember.bind(this)
     this.getBandSlideOneClasses = this.getBandSlideOneClasses.bind(this)
     this.getBandSlideTwoClasses = this.getBandSlideTwoClasses.bind(this)
     this.setCurrentBandSlide = this.setCurrentBandSlide.bind(this)
+    this.startBandSlide = this.startBandSlide.bind(this)
+    this.bandRightArrow = this.bandRightArrow.bind(this)
+    this.bandLeftArrow = this.bandLeftArrow.bind(this)
+    this.setBandSlide = this.setBandSlide.bind(this)
+    this.bandSlides = [
+      (
+        <div id="meet-sarah-container" style={{display: "flex", flexDirection: "row", alignItems: "center", justifyContent: "center"}}>
+              <div id="meet-sarah-image">
+              </div>
+              <div id="meet-sarah-text">
+                <span>Sarah</span>
+                <span onClick={this.setCurrentBandSlide} style={{cursor: "pointer"}}>Chad the first. Somthing Somthing something
+                Somthing Somthing somethingSomthing Somthing somethingSomthing Somthing somethingSomthing Somthing somethingSomthing Somthing something
+                Somthing Somthing somethingSomthing Somthing somethingSomthing Somthing somethingSomthing Somthing somethingSomthing Somthing something </span>
+              </div>
+        </div>
+      ),
+    (
+      <div id="meet-trevor-container" style={{display: "flex", flexDirection: "row", alignItems: "center", justifyContent: "center"}}>
+              <div id="meet-trevor-text">
+                <span>Trevor</span>
+                <span onClick={this.setCurrentBandSlide} style={{cursor: "pointer"}}>Chad the first. Somthing Somthing something
+                Somthing Somthing somethingSomthing Somthing somethingSomthing Somthing somethingSomthing Somthing somethingSomthing Somthing something
+                Somthing Somthing somethingSomthing Somthing somethingSomthing Somthing somethingSomthing Somthing somethingSomthing Somthing something </span>
+              </div>
+              <div id="meet-trevor-image">
+              </div>
+        </div>
+    ),
+    (
+      <div id="meet-zach-container" style={{display: "flex", flexDirection: "row", alignItems: "center", justifyContent: "center"}}>
+              <div id="meet-zach-image">
+              </div>
+              <div id="meet-zach-text">
+                <span>Zach</span>
+                <span onClick={this.setCurrentBandSlide} style={{cursor: "pointer"}}>Chad the first. Somthing Somthing something
+                Somthing Somthing somethingSomthing Somthing somethingSomthing Somthing somethingSomthing Somthing somethingSomthing Somthing something
+                Somthing Somthing somethingSomthing Somthing somethingSomthing Somthing somethingSomthing Somthing somethingSomthing Somthing something </span>
+              </div>
+        </div>
+    ),
+    (
+      <div id="meet-alex-container" style={{display: "flex", flexDirection: "row", alignItems: "center", justifyContent: "center"}}>
+              <div id="meet-alex-text">
+                <span>Alex</span>
+                <span onClick={this.setCurrentBandSlide} style={{cursor: "pointer"}}>Chad the first. Somthing Somthing something
+                Somthing Somthing somethingSomthing Somthing somethingSomthing Somthing somethingSomthing Somthing somethingSomthing Somthing something
+                Somthing Somthing somethingSomthing Somthing somethingSomthing Somthing somethingSomthing Somthing somethingSomthing Somthing something </span>
+              </div>
+              <div id="meet-alex-image">
+              </div>
+        </div>
+    )
+    ]
+    this.bandSlideLanding = (
+      <div id="meet-the-band-container" style={{display: "flex", flexDirection: "column", alignItems: "center", justifyContent: "center"}}>
+              <div id="meet-the-band-image">
+              </div>
+              <span id="meet-the-band-text" onClick={this.startBandSlide} style={{cursor: "pointer"}}>Meet the band --></span>
+      </div>
+    )
   }
 
   componentDidMount(){
     
 
+    this.setState({
+      bandSlide1: this.bandSlideLanding,
+      bandSlide2: this.bandSlides[0]
+    })
+    
     document.addEventListener("scroll", function(){
       let bandLocation = document.getElementById("band").getBoundingClientRect()
       bandLocation = bandLocation.bottom - bandLocation.height/2
@@ -45,35 +110,21 @@ export class App extends React.Component {
       let contactLocation = document.getElementById("contact").getBoundingClientRect()
       contactLocation = contactLocation.bottom - contactLocation.height/2
 
-      // if(bandLocation > 0 || bandLocation < window.innerHeight){
-      //   if((window.innerHeight/2) - bandLocation > 0){
-      //     this.setState({
-      //       bandOpacity: bandLocation / (window.innerHeight/2)
-      //     })
-      //   } else {
-      //     let x = window.innerHeight/2
-      //     this.setState({
-      //       bandOpacity: (x - (bandLocation - x)) / x
-      //     })
-      //   }
-      // }
-
       if(bandLocation > 40 && bandLocation < window.innerHeight-40){
-        // if((window.innerHeight/2) - bandLocation > 0){
-        //   this.setState({
-        //     bandOpacity: bandLocation / (window.innerHeight/2)
-        //   })
-        // } else {
-        //   let x = window.innerHeight/2
-        //   this.setState({
-        //     bandOpacity: (x - (bandLocation - x)) / x
-        //   })
-        // }
         document.getElementById("band").classList.remove("hide")
         document.getElementById("band").classList.add("show")
       } else {
         document.getElementById("band").classList.remove("show")
         document.getElementById("band").classList.add("hide")
+        if(this.state.currentBandIndex !== null && (bandLocation < - 200 || bandLocation > window.innerHeight + 200)){
+          this.setState({
+            currentBandIndex: null,
+            currentBandSlide: 1,
+            bandSlideDirection: "right",
+            bandSlide1: this.bandSlideLanding,
+            bandSlide2: this.bandSlides[0]
+          })
+        }
       }
 
       if(musicLocation > 0 && musicLocation < window.innerHeight){
@@ -110,52 +161,6 @@ export class App extends React.Component {
     }.bind(this));
   }
 
-  meetTheBand(){
-    this.setState({
-      currentBandMember: "sarah"
-    })
-  }
-
-  nextBandMember(){
-    if(this.state.currentBandMember === "sarah"){
-      this.setState({
-        currentBandMember: "trevor"
-      })
-    } else if (this.state.currentBandMember === "trevor"){
-      this.setState({
-        currentBandMember: "zach"
-      })
-    } else if (this.state.currentBandMember === "zach"){
-      this.setState({
-        currentBandMember: "alex"
-      })
-    } else if (this.state.currentBandMember === "alex"){
-      this.setState({
-        currentBandMember: "sarah"
-      })
-    }
-  }
-
-  previousBandMember(){
-    if(this.state.currentBandMember === "sarah"){
-      this.setState({
-        currentBandMember: "trevor"
-      })
-    } else if (this.state.currentBandMember === "trevor"){
-      this.setState({
-        currentBandMember: "zach"
-      })
-    } else if (this.state.currentBandMember === "zach"){
-      this.setState({
-        currentBandMember: "alex"
-      })
-    } else if (this.state.currentBandMember === "alex"){
-      this.setState({
-        currentBandMember: "sarah"
-      })
-    }
-  }
-
   getBandSlideOneClasses(){
     if (this.state.currentBandSlide === 1 && this.state.bandSlideDirection === "left"){
       return "right-show"
@@ -186,34 +191,60 @@ export class App extends React.Component {
     })
   }
 
+  setBandSlide(index, currentSlide){
+    if (currentSlide === 1){
+      this.setState({
+        bandSlide1: this.bandSlides[index]
+      })
+    } else{
+      this.setState({
+        bandSlide2: this.bandSlides[index]
+      })
+    }
+  }
+
+  startBandSlide(){
+    this.setCurrentBandSlide()
+    this.setState({
+      currentBandIndex: 0
+    })
+  }
+
+  bandRightArrow(){
+    let index = this.state.currentBandIndex === this.bandSlides.length -1 ? 0 : this.state.currentBandIndex + 1
+    this.setBandSlide(index, this.state.currentBandSlide === 1 ? 2 : 1)
+    this.setCurrentBandSlide()
+    this.setState({
+      currentBandIndex: index,
+      bandSlideDirection : "right"
+    })
+  }
+
+  bandLeftArrow(){
+    let index = this.state.currentBandIndex === 0 ? this.bandSlides.length - 1 : this.state.currentBandIndex - 1
+    this.setBandSlide(index, this.state.currentBandSlide === 1 ? 2 : 1)
+    this.setCurrentBandSlide()
+    this.setState({
+      currentBandIndex: index,
+      bandSlideDirection : "left"
+    })
+  }
+
   render(){
     return (
       <div className="App" style={{backgroundColor:"white"}}>
-        <div id="logo" style={{backgroundColor: "white", height: "400px", marginBottom: "200px"}}>
-          <img src="https://res.cloudinary.com/siouxcitymusic/image/upload/v1561436554/35928765_151362302397573_5730718706355404800_n.jpg" style={{height: "100%"}}/>
+        <div id="logo" style={{backgroundColor: "white", height: "100vh", marginBottom: "200px", display: "flex", alignItems: "center", justifyContent: "center"}}>
+          <img src="https://res.cloudinary.com/siouxcitymusic/image/upload/v1561436554/35928765_151362302397573_5730718706355404800_n.jpg" style={{height: "400px"}}/>
         </div>
         <div id="band" className="hide" style={{backgroundColor: "white", height: "400px", marginBottom: "200px"}}>
+        {this.state.currentBandIndex !== null?<span id="left-band-arrow" onClick={this.bandLeftArrow}>&lt;-</span>:""}
+        {this.state.currentBandIndex !== null?<span id="right-band-arrow" onClick={this.bandRightArrow}>-&gt;</span>:""}
           <div id="band-slide-1" style={{position: "absolute", marginLeft: "auto", marginRight: "auto", marginTop: "auto", marginBottom: "auto", left: "0", right: "0", top: "0", bottom: "0"}} className={this.getBandSlideOneClasses()}>
-            <div id="meet-the-band-container" style={{display: "flex", flexDirection: "column", alignItems: "center", justifyContent: "center"}}>
-              <div id="meet-the-band-image">
-              </div>
-              <span id="meet-the-band-text" onClick={this.setCurrentBandSlide} style={{cursor: "pointer"}}>Meet the band --></span>
-            </div>
+            {this.state.bandSlide1}
           </div>
           <div id="band-slide-2" style={{position: "absolute", marginLeft: "auto", marginRight: "auto", left: "0", right: "0"}} className={this.getBandSlideTwoClasses()}>
-            <div id="meet-sarah-container" style={{display: "flex", flexDirection: "row", alignItems: "center", justifyContent: "center"}}>
-              <div id="meet-sarah-image">
-              </div>
-              <div id="meet-sarah-text">
-                <span>Sarah</span>
-                <span onClick={this.setCurrentBandSlide} style={{cursor: "pointer"}}>Chad the first. Somthing Somthing something
-                Somthing Somthing somethingSomthing Somthing somethingSomthing Somthing somethingSomthing Somthing somethingSomthing Somthing something
-                Somthing Somthing somethingSomthing Somthing somethingSomthing Somthing somethingSomthing Somthing somethingSomthing Somthing something </span>
-              </div>
-              
-            </div>
+            {this.state.bandSlide2}
           </div>
-          
         </div>
         <div id="music" className="hide" style={{backgroundColor: "green", height: "400px", marginBottom: "400px"}}></div>
         <div id="photos" style={{backgroundColor: "blue", height: "400px", marginBottom: "400px"}}></div>
